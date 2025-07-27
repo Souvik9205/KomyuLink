3cab5c8da9b7be624e7eaad51a6cb156485b7b67
Souvik9205 <souvik9205@gmail.com>
2025-07-27 21:45:42 +0530

    server - web setup

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,14 +1,7 @@
-// apps/web/next.config.ts
-import path from "path";
-import { type NextConfig } from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config) {
-    // point `@/` at the app’s folder
-    config.resolve.alias ??= {};
-    config.resolve.alias["@"] = path.resolve(__dirname);
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
